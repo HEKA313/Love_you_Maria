f8fd717bc2ab4d55451425a5406b5ae3bbde96f8
HEKA313 <52586811+HEKA313@users.noreply.github.com>
2020-05-11 21:08:04 +0300

    Create minimum_line.py

---
+++ minimum_line.py
@@ -0,0 +1,22 @@
+N, M = input().split()
+N, M = int(N), int(M)
+a = []
+k = 0
+ind = 0
+
+for i in range(N):
+	a.append([int(j) for j in input(). split()])
+
+for i in range(N):
+	z = 0
+	for j in range(M):
+		z += a[i][j]
+	if i == 1:
+		k = z
+		continue
+	if z < k:
+		k = z
+		ind = i
+
+for row in a[ind]:
+	print(row, end=' ')
